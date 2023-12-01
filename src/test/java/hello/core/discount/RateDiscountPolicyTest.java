34c4a3896e24aa9fe9274bdf2eee7fa036ef019c
krkarma777 <krkarma777@gmail.com>
2023-12-01 16:17:33 +0900

    test : RateDiscountPolicyTest VIP가 아니면 할인 적용이 되지 않는지 테스트 추가

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -23,4 +23,12 @@ class RateDiscountPolicyTest {
         Assertions.assertThat(discount).isEqualTo(1000);
     }
 
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다.")
+    void vip_x(){
+        Member member = new Member(1L, "memberBasic", Grade.BASIC);
+        int discount = discountPolicy.discount(member, 10000);
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
 }
