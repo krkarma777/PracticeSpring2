c5670ed7adf78b83c0f9bc47913dc7820fe817b5
krkarma777 <krkarma777@gmail.com>
2023-12-03 01:28:36 +0900

    test: ApplicationContextSameBeanFindTest 특정 타입을 모두 조회하기

--- src/test/java/beanfind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/beanfind/ApplicationContextSameBeanFindTest.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,6 +31,18 @@ public class ApplicationContextSameBeanFindTest {
         MemberRepository memberRepository = ac.getBean("memberRepository1",MemberRepository.class);
         assertThat(memberRepository).isInstanceOf(MemberRepository.class);
     }
+
+    @Test
+    @DisplayName("특정 타입을 모두 조회하기.")
+    void findAllBeanByType(){
+        Map<String, MemberRepository> beansOfType = ac.getBeansOfType(MemberRepository.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value = " + beansOfType.get(key));
+        }
+        System.out.println("beansOfType = " + beansOfType);
+        assertThat(beansOfType.size()).isEqualTo(2);
+    }
+
 
     @Configuration
     static class SameBeanConfig {
