b9142011602fde12ce22db19946a7b169d9457ba
krkarma777 <krkarma777@gmail.com>
2023-12-03 01:18:59 +0900

    test: ApplicationContextSameBeanFindTest 테스트 성공을 위한 assertThrows 추가

--- src/test/java/beanfind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/beanfind/ApplicationContextSameBeanFindTest.java
@@ -5,9 +5,12 @@ import hello.core.member.MemoryMemberRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationContextSameBeanFindTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
@@ -16,6 +19,8 @@ public class ApplicationContextSameBeanFindTest {
     @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류가 발생한다.")
     void findBeanByTypeDuplicate() {
         MemberRepository bean = ac.getBean(MemberRepository.class);
+        assertThrows(NoUniqueBeanDefinitionException.class,
+                () -> ac.getBean(MemberRepository.class));
     }
 
     @Configuration
