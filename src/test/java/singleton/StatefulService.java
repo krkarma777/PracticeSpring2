13b0d1454a57496deda5f711abd57985b57796be
krkarma777 <krkarma777@gmail.com>
2023-12-04 00:02:46 +0900

    test: StatefulService, StatefulServiceTest
    싱글톤 패턴 문제 해결 방식 테스트

--- src/test/java/singleton/StatefulService.java
+++ src/test/java/singleton/StatefulService.java
@@ -4,12 +4,13 @@ public class StatefulService {
 
     private int price; //상태를 유지하는 필드
 
-    public void order(String name, int price) {
+    public int order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
-        this.price = price; // 문제 포인트
-    }
-
-    public int getPrice() {
+//        this.price = price; // 문제 포인트
         return price;
     }
+
+//    public int getPrice() {
+//        return price;
+//    }
 }
--- src/test/java/singleton/StatefulServiceTest.java
+++ src/test/java/singleton/StatefulServiceTest.java
@@ -17,15 +17,15 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         //ThreadA: A사용자 10000원 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
         //ThreadB: B사용자 20000원 주문
-        statefulService2.order("userB", 20000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
         //ThreadA: 사용자A 주문 금액 조회
-        int price = statefulService1.getPrice();
-        System.out.println("userA price = " + price);
+//        int price = statefulService1.getPrice();
+        System.out.println("userA price = " + userAPrice);
 
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        Assertions.assertThat(userAPrice).isEqualTo(10000);
     }
 
     static class TestConfig {
