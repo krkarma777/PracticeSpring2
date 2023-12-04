d374cd7feedc7124de5df8ace4d807ce6f0fe90a
krkarma777 <krkarma777@gmail.com>
2023-12-04 12:27:10 +0900

    fix: UnsatisfiedDependencyException 해결

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercernt = 10;
