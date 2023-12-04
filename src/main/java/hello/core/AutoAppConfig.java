eb8518a3f0742572b8d91eed50792b3e2aaab710
krkarma777 <krkarma777@gmail.com>
2023-12-04 12:26:24 +0900

    feat: AutoAppConfig test: AutoAppConfigTest
    component를 이용한 빈 자동 등록 및 테스트

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,12 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+}
