99dde13cedb289a4bf8c2bbda8bfbd3bf16d6670
krkarma777 <krkarma777@gmail.com>
2023-12-04 22:10:42 +0900

    feat: MainDiscountPolicy Qualifier 재정의 클래스 추가

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.MemberRepository;
@@ -10,13 +11,17 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
 
 
-    private final @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
